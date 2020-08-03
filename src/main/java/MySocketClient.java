a65d8d3aebac8ca899cd973d074bb3ffc1a7c4a3
youlanqiang <youlanqiang@hotmail.com>
2020-08-03 19:54:27 +0800

    完成Socket GUI

---
+++ src/main/java/MySocketClient.java
@@ -0,0 +1,122 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+/**
+ * author youlanqiang
+ */
+public class MySocketClient extends JFrame {
+
+    private JPanel panel;
+
+    //Host地址
+    private JTextField host;
+
+    //端口号
+    private JTextField port;
+
+    //消息输入栏
+    private JTextField message;
+
+    //连接按钮
+    private JButton connect;
+
+    //消息框
+    private JTextPane text;
+
+    //发送按钮
+    private JButton send;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MySocketClient frame = new MySocketClient();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MySocketClient() {
+        setTitle("客户端");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 440, 300);
+        setLocationRelativeTo(null);
+        panel = new JPanel();
+        panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(panel);
+
+        host = new JTextField();
+        host.setColumns(10);
+
+        port = new JTextField();
+        port.setColumns(10);
+
+        JLabel hostLabel = new JLabel("Host:");
+
+        JLabel portLabel = new JLabel("端口号:");
+
+        connect = new JButton("连接");
+
+        text = new JTextPane();
+
+        message = new JTextField();
+        message.setColumns(10);
+
+        send = new JButton("发送");
+        GroupLayout gl_panel = new GroupLayout(panel);
+        gl_panel.setHorizontalGroup(
+                gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_panel.createSequentialGroup()
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                                                .addGroup(gl_panel.createSequentialGroup()
+                                                        .addContainerGap()
+                                                        .addComponent(text))
+                                                .addGroup(GroupLayout.Alignment.LEADING, gl_panel.createSequentialGroup()
+                                                        .addComponent(hostLabel)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addComponent(host, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addComponent(portLabel)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addComponent(port, GroupLayout.PREFERRED_SIZE, 114, GroupLayout.PREFERRED_SIZE)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                                        .addComponent(connect)))
+                                        .addGroup(GroupLayout.Alignment.TRAILING, gl_panel.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(message, GroupLayout.DEFAULT_SIZE, 297, Short.MAX_VALUE)
+                                                .addGap(18)
+                                                .addComponent(send)))
+                                .addContainerGap())
+        );
+        gl_panel.setVerticalGroup(
+                gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_panel.createSequentialGroup()
+                                .addGap(19)
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(hostLabel)
+                                        .addComponent(host, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(portLabel)
+                                        .addComponent(port, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(connect))
+                                .addGap(18)
+                                .addComponent(text, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+                                .addGap(18)
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(message, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(send))
+                                .addContainerGap(14, Short.MAX_VALUE))
+        );
+        panel.setLayout(gl_panel);
+    }
+}
---
+++ src/main/java/MySocketServer.java
@@ -0,0 +1,108 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
+
+
+public class MySocketServer extends JFrame {
+
+
+    private JPanel panel;
+
+    //端口号
+    private JTextField port;
+
+    //消息栏
+    private JTextField message;
+
+    //启动按钮
+    private  JButton start;
+
+    //消息框
+    private JTextPane text;
+
+    //发送
+    private JButton send;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MySocketServer frame = new MySocketServer();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MySocketServer() {
+        setTitle("服务器");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 392, 300);
+        setLocationRelativeTo(null);
+        panel = new JPanel();
+        panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(panel);
+
+        JLabel portLabel = new JLabel("端口号:");
+
+        port = new JTextField();
+        port.setColumns(10);
+
+        start = new JButton("启动");
+
+         text = new JTextPane();
+
+        message = new JTextField();
+        message.setColumns(10);
+
+        send = new JButton("发送");
+        GroupLayout gl_panel = new GroupLayout(panel);
+        gl_panel.setHorizontalGroup(
+                gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_panel.createSequentialGroup()
+                                .addGap(19)
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(gl_panel.createSequentialGroup()
+                                                .addComponent(message, GroupLayout.PREFERRED_SIZE, 247, GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(send)
+                                                .addGap(1))
+                                        .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                                .addGroup(gl_panel.createSequentialGroup()
+                                                        .addComponent(text, GroupLayout.PREFERRED_SIZE, 328, GroupLayout.PREFERRED_SIZE)
+                                                        .addContainerGap())
+                                                .addGroup(gl_panel.createSequentialGroup()
+                                                        .addComponent(portLabel)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                        .addComponent(port, GroupLayout.PREFERRED_SIZE, 194, GroupLayout.PREFERRED_SIZE)
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                                                        .addComponent(start)
+                                                        .addGap(31)))))
+        );
+        gl_panel.setVerticalGroup(
+                gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_panel.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(portLabel)
+                                        .addComponent(port, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(start))
+                                .addGap(20)
+                                .addComponent(text, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                                .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(message, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(send))
+                                .addContainerGap())
+        );
+        panel.setLayout(gl_panel);
+    }
+}

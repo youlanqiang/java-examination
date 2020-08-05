31cfdc051a1788bcef5a82b5a333aa3db36523e6
游兰强 <youlanqiang@hotmail.com>
2020-08-05 11:21:33 +0800

    基于SwingGUI的Socket通讯

--- src/main/java/MyClientSocketGUI.java
+++ src/main/java/MyClientSocketGUI.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * author youlanqiang
  */
-public class MySocketClient extends JFrame {
+public class MyClientSocketGUI extends JFrame {
 
     private JPanel panel;
 
@@ -23,21 +28,23 @@ public class MySocketClient extends JFrame {
     private JButton connect;
 
     //消息框
-    private JTextPane text;
+    private JTextArea text;
 
     //发送按钮
     private JButton send;
 
-    private Socket socket;
+    private MyClientSocket clientSocket;
+
 
     /**
      * Launch the application.
      */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 try {
-                    MySocketClient frame = new MySocketClient();
+                    MyClientSocketGUI frame = new MyClientSocketGUI();
                     frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -49,8 +56,7 @@ public class MySocketClient extends JFrame {
     /**
      * Create the frame.
      */
-    public MySocketClient() {
-        socket = new Socket();
+    public MyClientSocketGUI() {
 
         setTitle("客户端");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,22 +68,60 @@ public class MySocketClient extends JFrame {
 
         host = new JTextField();
         host.setColumns(10);
+        host.setText("localhost");
 
         port = new JTextField();
         port.setColumns(10);
+        port.setText("855");
 
         JLabel hostLabel = new JLabel("Host:");
 
         JLabel portLabel = new JLabel("端口号:");
 
         connect = new JButton("连接");
+        connect.addActionListener((e)->{
+            if(connect.getText().equals("连接")){
+                try {
+                    clientSocket = new MyClientSocket();
+                    clientSocket.initSocket(host.getText(), Integer.parseInt(port.getText()));
+                    clientSocket.send("连接成功");
+                    text.append("连接成功\n");
+                    message.setEditable(true);
+                    send.setEnabled(true);
+                    host.setEditable(false);
+                    port.setEditable(false);
+                    connect.setText("断开");
+                } catch (Exception ex) {
+                    text.append("连接失败\n");
+                }
+            }else{
+                clientSocket.close();
+                text.append("关闭成功\n");
+                message.setEditable(false);
+                send.setEnabled(false);
+                host.setEditable(true);
+                port.setEditable(true);
+                connect.setText("连接");
+            }
+        });
 
-        text = new JTextPane();
+        text = new JTextArea();
+        JScrollPane textPane = new JScrollPane(text);
 
         message = new JTextField();
         message.setColumns(10);
+        message.setEditable(false);
 
         send = new JButton("发送");
+        send.setEnabled(false);
+        send.addActionListener((x)->{
+            String messageValue = message.getText();
+            text.append("本机:"+messageValue+"\n");
+            clientSocket.send(messageValue);
+            message.setText("");
+        });
+
+
         GroupLayout gl_panel = new GroupLayout(panel);
         gl_panel.setHorizontalGroup(
                 gl_panel.createParallelGroup(GroupLayout.Alignment.LEADING)
@@ -86,7 +130,7 @@ public class MySocketClient extends JFrame {
                                         .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
                                                 .addGroup(gl_panel.createSequentialGroup()
                                                         .addContainerGap()
-                                                        .addComponent(text))
+                                                        .addComponent(textPane))
                                                 .addGroup(GroupLayout.Alignment.LEADING, gl_panel.createSequentialGroup()
                                                         .addComponent(hostLabel)
                                                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
@@ -115,7 +159,7 @@ public class MySocketClient extends JFrame {
                                         .addComponent(port, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                         .addComponent(connect))
                                 .addGap(18)
-                                .addComponent(text, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(textPane, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
                                 .addGap(18)
                                 .addGroup(gl_panel.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(message, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
@@ -124,4 +168,71 @@ public class MySocketClient extends JFrame {
         );
         panel.setLayout(gl_panel);
     }
+
+
+    public class MyClientSocket extends Thread{
+
+        private Socket socket;
+        private OutputStream outputStream;
+
+        public void initSocket(String socketHost, int socketPort) throws IOException {
+            socket = new Socket(socketHost, socketPort);
+            outputStream = socket.getOutputStream();
+
+            Thread receivingThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null){
+                            if(line.equals("服务器关闭")){
+                                clientSocket.close();
+                                text.append("已断开连接，服务器关闭\n");
+                                message.setEditable(false);
+                                send.setEnabled(false);
+                                host.setEditable(true);
+                                port.setEditable(true);
+                                connect.setText("连接");
+                                return;
+                            }else {
+                                text.append(line + CRLF);
+                            }
+
+                        }
+
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            };
+            receivingThread.start();
+        }
+
+        private static final String CRLF = "\r\n"; // newline
+
+        /** Send a line of text */
+        public void send(String message) {
+            try {
+                outputStream.write((message + CRLF).getBytes());
+                outputStream.flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        /** Close the socket */
+        public void close() {
+            try {
+                send("退出连接");
+                socket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
